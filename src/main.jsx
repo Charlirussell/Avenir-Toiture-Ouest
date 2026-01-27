033faac2b269a591a45ba71478737a97cf641fcb
Charli Russell <charlirussell2491@gmail.com>
2026-01-27 01:27:25 +0100

    Add global Sass styles structure

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import ReactDOM from "react-dom/client";
 import App from "./components/App/App.jsx";
 
+import  './styles/index.scss';
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <App />
 );
