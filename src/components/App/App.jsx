37903c5eb275ebe539baa7da898d02ee18eeeb0d
Charli Russell <charlirussell2491@gmail.com>
2026-02-04 00:13:31 +0100

    Add footer component and render it globally

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,6 @@
 import { Routes, Route } from 'react-router-dom';
 
+import Footer from '../Footer/Footer';
 import UnderConstruction from '../UnderConstruction/UnderConstrution';
 
 import './App.scss';
@@ -10,9 +11,12 @@ const App = () => {
     import.meta.env.VITE_UNDER_CONSTRUCTION === "true";
     
   return (
-    <Routes>
-      <Route path="*" element={<UnderConstruction />} />
-    </Routes>
+    <>
+      <Routes>
+        <Route path="*" element={<UnderConstruction />} />
+      </Routes>
+      <Footer />
+    </>
   );
 };
 
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+import './UnderConstruction.scss';
+
+const Footer = () => {
+    return (
+        <></>
+    )
+}
+
+export default Footer;
