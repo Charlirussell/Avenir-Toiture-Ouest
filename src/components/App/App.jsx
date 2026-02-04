218e87f8aaedfce0eb6b2651d5e676a2ba94c871
Charli Russell <charlirussell2491@gmail.com>
2026-02-04 01:45:05 +0100

    Fix under construction routing and added Home component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,8 @@
 import { Routes, Route } from 'react-router-dom';
 
+import Home from '../Home/Home';
 import Footer from '../Footer/Footer';
-import UnderConstruction from '../UnderConstruction/UnderConstrution';
+import UnderConstruction from '../UnderConstruction/UnderConstruction';
 
 import './App.scss';
 
@@ -11,12 +12,21 @@ const App = () => {
     import.meta.env.VITE_UNDER_CONSTRUCTION === "true";
     
   return (
-    <>
-      <Routes>
-        <Route path="*" element={<UnderConstruction />} />
-      </Routes>
-      <Footer />
-    </>
+    <div className="app">
+      {isUnderConstruction ? (
+        <Routes>
+          <Route path="*" element={<UnderConstruction />} />
+        </Routes>
+      ) : (
+        <>
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+
+          <Footer />
+        </>
+      )}
+    </div>
   );
 };
 
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,6 +3,9 @@ import './Footer.scss';
 const Footer = () => {
     return (
         <>
+        <div className='footer'>
+            <div><p>&copy; 2024 <a href="https://fusiondigital.fr" target="_blank" rel="noopener noreferrer">Fusion Digital</a>. All rights reserved.</p></div>
+        </div>
         </>
     )
 }
