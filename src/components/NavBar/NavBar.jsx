64cd02a6e6f37c269a7d7de41a8199b7eb8e7acc
Charli Russell <charlirussell2491@gmail.com>
2026-03-11 19:39:42 +0100

    Added banner to navbar added scss to banner and navbar to position the banner correctly

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,5 +1,6 @@
 import { NavLink } from 'react-router-dom';
 import { useEffect, useState } from 'react';
+import Banner from '../../assets/images/Banner-ATO.svg';
 
 import './NavBar.scss';
 
@@ -25,25 +26,30 @@ const NavBar = () => {
     }, []);
 
     return (
-        <nav className='navbar'>
-            {/*Hamburger Buttoon*/}
-            <button
-                type='button'
-                className='hamburger'
-                onClick={toggleMenu}
-                aria-expanded={isOpen}
-                aria-label={isOpen ? 'close' : 'open'}
-            >
-                ☰
-            </button>
-            <div className={`navlinks ${isOpen ? 'is-open' : ''}`}>
-                <NavLink to="/"className={({ isActive }) =>isActive ? "menu menu--active" : "menu"}onClick={handleLinkClick}>Accueil</NavLink>
-                <NavLink to="/services"className={({ isActive }) =>isActive ? "menu menu--active" : "menu"}onClick={handleLinkClick}>Nos Services</NavLink>
-                <NavLink to="/realisations"className={({ isActive }) =>isActive ? "menu menu--active" : "menu"}onClick={handleLinkClick}>Réalisations</NavLink>
-                <NavLink to="/reviews"className={({ isActive }) =>isActive ? "menu menu--active" : "menu"}onClick={handleLinkClick}>Avis</NavLink>
-                <NavLink to="/contact" className="small-button" onClick={handleLinkClick}>Contact</NavLink>
-            </div>
-        </nav>
+        <div className='navbar-wrap'>
+            <NavLink to='/' className='banner'>
+                <img src={Banner} alt="Navbar Logo" />
+            </NavLink>
+            <nav className='navbar'>
+                {/*Hamburger Buttoon*/}
+                <button
+                    type='button'
+                    className='hamburger'
+                    onClick={toggleMenu}
+                    aria-expanded={isOpen}
+                    aria-label={isOpen ? 'close' : 'open'}
+                >
+                    ☰
+                </button>
+                <div className={`navlinks ${isOpen ? 'is-open' : ''}`}>
+                    <NavLink to="/"className={({ isActive }) =>isActive ? "menu menu--active" : "menu"}onClick={handleLinkClick}>Accueil</NavLink>
+                    <NavLink to="/services"className={({ isActive }) =>isActive ? "menu menu--active" : "menu"}onClick={handleLinkClick}>Nos Services</NavLink>
+                    <NavLink to="/realisations"className={({ isActive }) =>isActive ? "menu menu--active" : "menu"}onClick={handleLinkClick}>Réalisations</NavLink>
+                    <NavLink to="/reviews"className={({ isActive }) =>isActive ? "menu menu--active" : "menu"}onClick={handleLinkClick}>Avis</NavLink>
+                    <NavLink to="/contact" className="small-button" onClick={handleLinkClick}>Contact</NavLink>
+                </div>
+            </nav>
+        </div>
     )
 }
 
