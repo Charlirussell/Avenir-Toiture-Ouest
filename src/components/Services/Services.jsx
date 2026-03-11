4a53a0d3beb0dad11464fd703a5dc35d1e655529
Charli Russell <charlirussell2491@gmail.com>
2026-03-11 22:14:30 +0100

    Reuploaded the services icons to the correct images. >> Added serivces text >> Imported the images and added appropritate sass for centering all services items

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,9 +1,21 @@
+import ServicesOne from '../../assets/icons/repairs-icon.svg';
+import ServicesTwo from '../../assets/icons/gutters-icon.svg';
+import ServicesThree from '../../assets/icons/maintenance-icon.svg';
+
 import './Services.scss';
 
 const Services = () => {
     return (
         <section id='services' className='services'>
-            <h1 className='services-header'>Nos Services</h1>
+            <div className='services-content'>
+                <h1 className='services-header'>Nos Services</h1>
+                <p className='services-paragraph'>Basée à Carhaix-Plouguer, Avenir Toiture Ouest réalise vos travaux de toiture et de zinguerie en Bretagne : révision, réparation et entretien de toitures en ardoise, ainsi que pose et réparation de gouttières.</p>
+            <div className='services-icons'>
+                <img className='services-one' src={ServicesOne} alt="repairs icon" />
+                <img className='services-two' src={ServicesTwo} alt="gutters icon" />
+                <img className='services-three' src={ServicesThree} alt="maintenance icon" />
+            </div>
+            </div>
         </section>
     )
 }
