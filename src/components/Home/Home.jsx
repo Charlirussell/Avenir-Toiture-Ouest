d406f57add34624417ddd8f699442a8975c9b437
Charli Russell <charlirussell2491@gmail.com>
2026-03-11 12:44:54 +0100

    Added background image and imported image to home page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,11 @@
+import HomeImg from '../../assets/images/Home_Background.webp';
+
 import './Home.scss';
 
 const Home = () => {
     return (
-        <div className='home'>
-            <h1>Hello World</h1>
+        <div className='home-main'>
+            <img src={HomeImg} alt='home-img' />
         </div>
     )
 }
