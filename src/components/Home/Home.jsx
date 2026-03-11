d53c2994a323d25de369b1981aecf8ae70bfa727
Charli Russell <charlirussell2491@gmail.com>
2026-03-11 19:01:45 +0100

    Added home text and buttons, added scss to text and buttons

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -4,10 +4,20 @@ import './Home.scss';
 
 const Home = () => {
     return (
-        <div className='home-main'>
+        <section className='home'>
             <img className='home-img' src={HomeImg} alt='home background' />
-        </div>
+            <div className='home-content'>
+                <h1 className='header'>Votre couvreur en Bretagne</h1>
+                <p className='header-paragraph'><span>Avenir Toiture Ouest</span> - Artisan couvreur à votre service pour tous vos travaux de toiture : réparation, zinguerie, entretien et pose de gouttières à Carhaix-Plouguer et aux alentours.</p>
+            <div className='home-buttons'>
+                <button className='small-button'>Devis Gratuit</button>
+                <button className='phone'><a href='tel:0780627064'></a>07 80 62 70 64</button>
+                <button className='services'>Nos Services</button>
+            </div>
+            </div>
+        </section>
     )
 }
 
 export default Home;
+
