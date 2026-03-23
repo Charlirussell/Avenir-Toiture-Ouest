1a5b4b3d638d115c54e900b6ab44e3f0f8f35f41
Charli Russell <charlirussell2491@gmail.com>
2026-03-23 20:02:05 +0100

    Adjusted App routing so that Privacy page can have it's own page. Imported and added privacy to routes

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,9 +1,11 @@
+import { Routes, Route } from 'react-router-dom';
 import NavBar from '../NavBar/NavBar';
 import Home from '../Home/Home';
 import Services from '../Services/Services';
 import Contact from '../Contact/Contact'
 import Reviews from '../Reviews/Reviews';
 import Footer from '../Footer/Footer';
+import Privacy from '../PrivacyPolicy/Privacy';
 import UnderConstruction from '../UnderConstruction/UnderConstruction';
 
 import './App.scss';
@@ -15,23 +17,40 @@ const App = () => {
   const isUnderConstruction =
     import.meta.env.VITE_UNDER_CONSTRUCTION === "false";
     
-  return (
-    <div className="app">
-          {isUnderConstruction ? (
-            <UnderConstruction />
-          ) : (
-            <>
-              <NavBar />
-              <Home />
-              <Services />
-              <Contact />
-              <Reviews />
-              <Footer />
-            </>
-          )}
-        </div>
-      );
-    };
+      return (
+          <div className="app">
+
+            {isUnderConstruction ? (
+              <UnderConstruction />
+            ) : (
+
+              <Routes>
+
+                {/* MAIN ONE-PAGE SITE */}
+                <Route
+                  path="/"
+                  element={
+                    <>
+                      <NavBar />
+                      <Home />
+                      <Services />
+                      <Contact />
+                      <Reviews />
+                      <Footer />
+                    </>
+                  }
+                />
+                <Route
+                  path="/mentions-legales"
+                  element={<Privacy />}
+                />
+              </Routes>
+
+            )}
+
+          </div>
+        );
+      };
 
 
 export default App;
