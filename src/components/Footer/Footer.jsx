44fa17dcbc232dc144af8415a4330eba8c60b67f
Charli Russell <charlirussell2491@gmail.com>
2026-03-23 11:43:36 +0100

    Added links to the footer, added relevent scss for styling

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,11 +2,25 @@ import './Footer.scss';
 
 const Footer = () => {
     return (
-        <>
-        <div className='footer'>
-            <div><p>&copy; 2025 <a href="https://fusiondigital.fr" target="_blank" rel="noopener noreferrer">Fusion Digital</a>. All rights reserved.</p></div>
-        </div>
-        </>
+    <footer id='footer' className='footer'>
+        <h2 className='footer-title'>Liens</h2>
+        <ul className='footer-links'>
+            <li><a href="https://www.facebook.com/avenirtoitureouest" target="_blank" rel="noopener noreferrer">Facebook</a></li>
+            <li><a href='#home'>Accueil</a></li>
+            <li><a href='#services'>Nos Services</a></li>
+            <li><a href='#contact'>Contact</a></li>
+        </ul>
+        <p className="footer-copyright">
+        © 2025 All Rights Reserved.{" "}
+        <a
+            href="https://fusiondigital.fr"
+            target="_blank"
+            rel="noopener"
+        >
+            Création site web par Fusion Digital
+        </a>
+        </p>
+    </footer>
     )
 }
 
