fb828d6c18b43c7876043077d527fc8af9135038
Charli Russell <charlirussell2491@gmail.com>
2026-03-23 17:46:51 +0100

    Added reviews array

--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -5,8 +5,27 @@ import 'slick-carousel/slick/slick-theme.css';
 
 import './Reviews.scss';
 
+const reviewsData = [
+    {
+        id: 1,
+        name: 'Philip Larbalestier',
+        Location: 'Carnoët',
+        rating: 5,
+        text: 'Great work from Dylan! He removed all the ivy from the roof, replaced the broken slates, and fixed a leak coming from the gable. Everything was completed efficiently and to a high standard. Professional, reliable, and easy to deal with. I highly recommend him for any roofing work.',
+
+    },
+    {
+       id: 2,
+        name: 'Alexis Lr',
+        Location: 'Plounévézel',
+        rating: 5,
+        text: "Je suis très content de la prestation effectuée pour une réparation de fuite avec remplacement d'ardoises et de plusieurs tuiles faîtière , je recommande cette entreprise grandement.",
+
+    },
+];
+
 const Reviews = () => {
-    
+     
     return (
         <section id='reviews' className='reviews'>
             <h2 className='reviews-title'>Avis de Nos Clients</h2>
