47fe369eaa1295703bc06ac5827cbd45b25680d7
Charli Russell <charlirussell2491@gmail.com>
2026-03-23 19:26:14 +0100

    Added Privacy component

---
+++ src/components/PrivacyPolicy/Privacy.jsx
@@ -0,0 +1,11 @@
+import './Privacy.scss';
+
+const Privacy = () => {
+    return (
+        <section id='privacy' className='Privacy'>
+            
+        </section>
+    )
+}
+
+export default Privacy;
